36e32f70d2e215f2d4bd0ff2dbbe4fa37bd0fee4
MAKALAOSIBUSISO <sbusisomakalo@gmail.com>
2023-02-17 09:34:09 -0800

    Numberz

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-/**
- * main - Write a program that prints the lowercase alphabet in reverse, followed by a new line.
- *
- * Return: Always 0 (Success)
- */
 
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char c;
+	char letter;
 
-	c = 'z';
-	while
-		(c >= 'a') {
-			putchar(c);
-			c--;
-		}
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
