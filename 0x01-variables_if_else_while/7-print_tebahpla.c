5e145767ad97408413e14e090f34ad27410f8f17
MAKALAOSIBUSISO <sbusisomakalo@gmail.com>
2023-02-17 06:18:20 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
