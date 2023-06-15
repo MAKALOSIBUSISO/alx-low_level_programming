d0f938f550ba707254e1cb573e86644e9185cad1
MAKALOSIBUSISO <sbusisomakalo@gmail.com>
2023-06-15 11:13:21 -0700

    makalo

--- 0x17-doubly_linked_lists/1-dlisint_len.c
+++ 0x17-doubly_linked_lists/1-dlisint_len.c
@@ -1,28 +1,19 @@
 #include "lists.h"
 
 /**
- * dl int_len - returns the number of elements in a double linked list
+ * dlint_len - returns the number of elements in a double linked list
  *
  * @h: The head of the list
  * Return: Number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		i++;
 		h = h->next;
 	}
-
-	return (count);
+	return (i);
 }
